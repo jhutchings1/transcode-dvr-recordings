0ab3a25712ecace6cebdccb848317910a239c182
Justin Hutchings <jhutchings1@users.noreply.github.com>
2021-10-22 21:45:32 -0700

    Add finished directory

--- transcode.ps1
+++ transcode.ps1
@@ -3,6 +3,13 @@ cd $dvrRoot
 
 # Loops through all of the files in the $dvrRoot directory where the extension is .mpg
 # Calls ffmpeg to convert from mpeg-2 to mpeg-4
-foreach ($file in (Get-ChildItem -Recurse | Where-Object {$_.Extension -eq ".mpg"})) {
+foreach ($file in (Get-ChildItem -Recurse | Where-Object {$_.Extension -eq ".mpg" -and $_.DirectoryName -notcontains "finished"})) {
+  # Create a directory for finished files
+  $finished = New-Item -Path "$($file.DirectoryName)/finished" -ItemType Directory -ErrorAction SilentlyContinue
+
+  #Convert the file
   Invoke-Expression "ffmpeg -i `"$($file.Name)`" `"$($file.BaseName).mp4`""
+  
+  # Move the old file to the finished directory
+  Move-Item -Path $file.Name -Destination "$($finished.FullName)/$($file.Name)"
 }
