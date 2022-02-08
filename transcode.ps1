b25b4ba3f275e9f63fb0005c8f1fa08fe1fcac02
Justin Hutchings <jhutchings1@users.noreply.github.com>
2022-02-07 20:25:52 -0800

    Add parameter to script

--- transcode.ps1
+++ transcode.ps1
@@ -1,14 +1,15 @@
-$dvrRoot = "" # Set the appropriate root directory for your DVR here
-cd $dvrRoot
+param(
+  [string] $path
+)
 
 # Loops through all of the files in the $dvrRoot directory where the extension is .mpg
 # Calls ffmpeg to convert from mpeg-2 to mpeg-4
-foreach ($file in (Get-ChildItem -Recurse | Where-Object {$_.Extension -eq ".mpg" -and $_.DirectoryName -notcontains "finished"})) {
+foreach ($file in (Get-ChildItem -Path $path -Recurse | Where-Object {$_.Extension -eq ".mpg" -and $_.DirectoryName -notcontains "finished"})) {
   # Create a directory for finished files
   $finished = New-Item -Path "$($file.DirectoryName)/finished" -ItemType Directory -ErrorAction SilentlyContinue
 
   #Convert the file
-  Invoke-Expression "ffmpeg -i `"$($file.Name)`" `"$($file.BaseName).mp4`" -n" 
+  Invoke-Expression "ffmpeg -i `"$($file.FullName)`" `"$($finished.FullName)/$($file.BaseName).mp4`" -n" 
   
   # Move the old file to the finished directory
   Move-Item -Path $file.Name -Destination "$($finished.FullName)/$($file.Name)"
