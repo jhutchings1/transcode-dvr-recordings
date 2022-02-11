51af6f1bdaa7f29aa9013c29396425dbafc6003f
Justin Hutchings <jhutchings1@users.noreply.github.com>
2022-02-10 19:58:08 -0800

    Fix issue with more than 1 invocation

--- transcode.ps1
+++ transcode.ps1
@@ -5,9 +5,13 @@ param(
 # Loops through all of the files in the $dvrRoot directory where the extension is .mpg
 # Calls ffmpeg to convert from mpeg-2 to mpeg-4
 foreach ($file in (Get-ChildItem -Path $path -Recurse | Where-Object {$_.Extension -eq ".mpg" -and $_.DirectoryName -notcontains "finished"})) {
-  # Create a directory for finished files
-  $finished = New-Item -Path "$($file.DirectoryName)/finished" -ItemType Directory -ErrorAction SilentlyContinue
-
+  $finished = $null
+  if (Test-Path "$($file.DirectoryName)/finished") {
+    $finished = Get-Item -Path "$($file.DirectoryName)/finished"
+  } else {
+    # Create a directory for finished files
+    $finished = New-Item -Path "$($file.DirectoryName)/finished" -ItemType Directory 
+  }
   #Convert the file
   Invoke-Expression "ffmpeg -i `"$($file.FullName)`" `"$($finished.FullName)/$($file.BaseName).mp4`" -n" 
 }
