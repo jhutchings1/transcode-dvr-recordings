dc9ad9acf84df2279602fc45f4de374c1ab18a27
Justin Hutchings <jhutchings1@users.noreply.github.com>
2022-02-18 08:38:55 -0800

    Add support for resuming on a partially converted folder

--- transcode.ps1
+++ transcode.ps1
@@ -2,16 +2,25 @@ param(
   [string] $path
 )
 
+function createFinishedPath($file) {
+  $finishedPath = $null
+  if (Test-Path "$($file.DirectoryName)/finished") {
+    $finishedPath = Get-Item -Path "$($file.DirectoryName)/finished"
+  } else {
+    # Create a directory for finished files
+    $finishedPath = New-Item -Path "$($file.DirectoryName)/finished" -ItemType Directory 
+  }
+  return $finishedPath
+}
+
 # Loops through all of the files in the $dvrRoot directory where the extension is .mpg
 # Calls ffmpeg to convert from mpeg-2 to mpeg-4
 foreach ($file in (Get-ChildItem -Path $path -Recurse | Where-Object {$_.Extension -eq ".mpg" -and $_.DirectoryName -notcontains "finished"})) {
-  $finished = $null
-  if (Test-Path "$($file.DirectoryName)/finished") {
-    $finished = Get-Item -Path "$($file.DirectoryName)/finished"
-  } else {
-    # Create a directory for finished files
-    $finished = New-Item -Path "$($file.DirectoryName)/finished" -ItemType Directory 
+  $finishedPath = createFinishedPath($file)
+  
+  if (!(Test-Path "$($finishedPath.FullName)/$($file.BaseName).mp4")) {
+    #Convert the file
+    Invoke-Expression "ffmpeg -i `"$($file.FullName)`" `"$($finishedPath.FullName)/$($file.BaseName).mp4`" -n" 
   }
-  #Convert the file
-  Invoke-Expression "ffmpeg -i `"$($file.FullName)`" `"$($finished.FullName)/$($file.BaseName).mp4`" -n" 
+  
 }
